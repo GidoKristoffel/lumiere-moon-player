6b76741b452899206a916200dfdcd4c060a567d4
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-05 20:41:13 +0300

    ⚰️ Removed default layout, styles and assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { invoke } from "@tauri-apps/api/tauri";
 
 @Component({
   selector: 'app-root',
@@ -11,14 +10,4 @@ import { invoke } from "@tauri-apps/api/tauri";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  greetingMessage = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
 }
