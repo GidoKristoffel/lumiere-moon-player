e19f5f985f1b8bcc549c8fef6b21e64a1c073288
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-08 20:24:51 +0300

    👔 Updated icon button

--- src/app/shared/components/buttons/icon-btn/icon-btn.component.ts
+++ src/app/shared/components/buttons/icon-btn/icon-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
 
 @Component({
@@ -14,4 +14,9 @@ export class IconBtnComponent {
   @Input() btnSize: string = '';
   @Input() icon: string = '';
   @Input() iconSize: string = '';
+  @Output() click: EventEmitter<void> = new EventEmitter();
+
+  public onClick(): void {
+    this.click.emit();
+  }
 }
