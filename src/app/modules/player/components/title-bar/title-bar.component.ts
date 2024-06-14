90963945864b89532697e4f208dfe609f7f0be04
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-14 17:13:32 +0300

    👔 Added the ability to minimize and maximize the window to full screen

--- src/app/modules/player/components/title-bar/title-bar.component.ts
+++ src/app/modules/player/components/title-bar/title-bar.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
 import { IconBtnComponent } from "../../../../shared/components/buttons/icon-btn/icon-btn.component";
 import { WindowService } from "../../../../core/services/window/window.service";
+import {
+  IconToggleBtnComponent
+} from "../../../../shared/components/buttons/icon-toggle-btn/icon-toggle-btn.component";
 
 @Component({
   selector: 'lmp-title-bar',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    IconBtnComponent
+    IconBtnComponent,
+    IconToggleBtnComponent
   ],
   templateUrl: './title-bar.component.html',
   styleUrl: './title-bar.component.css'
@@ -16,8 +20,15 @@ import { WindowService } from "../../../../core/services/window/window.service";
 export class TitleBarComponent {
   constructor(private windowService: WindowService) {}
 
-  closeWindow(): void {
-    console.log('click!!!');
+  public minimizeWindow(): void {
+    this.windowService.minimize();
+  }
+
+  public fullscreenWindow(fullscreen: boolean): void {
+    this.windowService.setFullscreen(fullscreen);
+  }
+
+  public closeWindow(): void {
     this.windowService.close();
   }
 }
