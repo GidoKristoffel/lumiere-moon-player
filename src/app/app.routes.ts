9ba8c448558191f8d53860b50b5de14bd7ae8b92
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-15 20:12:14 +0300

    👔 Updated styles and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'player',
+        loadComponent: () => import('./pages/player-page/player-page.component').then((c) => c.PlayerPageComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'player',
+    }
+];
