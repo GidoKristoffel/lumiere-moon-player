b3f299221d8085f96271429194982ffba282080f
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-16 19:46:11 +0300

    👔 Added video service

---
+++ src/app/core/services/video.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideoService {
+  private video: File | undefined;
+
+  constructor() { }
+
+  public get(): File | undefined {
+    return this.video;
+  }
+
+  public set(video: File): void {
+    this.video = video;
+  }
+}
