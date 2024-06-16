95617771419a316ace1ea38700f37183934c7ca8
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-16 20:11:40 +0300

    👔 Added guards to check for video availability and updated routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,26 @@
 import { Routes } from "@angular/router";
+import { videoAvailableGuard } from "./core/guards/video-available/video-available.guard";
+import { videoUnavailableGuard } from "./core/guards/video-unavailable/video-unavailable.guard";
 
 export const routes: Routes = [
     {
         path: 'player',
-        loadComponent: () => import('./pages/player-page/player-page.component').then((c) => c.PlayerPageComponent)
+        loadComponent: () => import('./pages/player-page/player-page.component').then((c) => c.PlayerPageComponent),
+        canActivate: [videoAvailableGuard]
     },
     {
         path: 'settings',
-        loadComponent: () => import('./pages/settings-page/settings-page.component').then((c) => c.SettingsPageComponent)
+        loadComponent: () => import('./pages/settings-page/settings-page.component').then((c) => c.SettingsPageComponent),
+        canActivate: [videoAvailableGuard]
     },
     {
         path: 'video-selection',
-        loadComponent: () => import('./pages/video-selection-page/video-selection-page.component').then((c) => c.VideoSelectionPageComponent)
+        loadComponent: () => import('./pages/video-selection-page/video-selection-page.component').then((c) => c.VideoSelectionPageComponent),
+        canActivate: [videoUnavailableGuard]
     },
     {
         path: '',
         redirectTo: 'player',
+        pathMatch: "full"
     }
 ];
--- src/app/pages/player-page/player-page.component.ts
+++ src/app/pages/player-page/player-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ControlPanelComponent } from "../../modules/player/components/control-panel/control-panel.component";
+import { TitleBarComponent } from "../../modules/player/components/title-bar/title-bar.component";
 
 @Component({
   selector: 'lmp-player-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ControlPanelComponent,
+    TitleBarComponent
+  ],
   templateUrl: './player-page.component.html',
   styleUrl: './player-page.component.css'
 })
