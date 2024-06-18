cc8ebdbff95630bf080e7b588d1ff6f3cd04fe92
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-18 16:44:56 +0300

    👔 Added default button component

--- src/app/modules/video-selection/components/video-selection/video-selection.component.ts
+++ src/app/modules/video-selection/components/video-selection/video-selection.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DefaultBtnComponent } from "../../../../shared/components/buttons/default-btn/default-btn.component";
 
 @Component({
   selector: 'lmp-video-selection',
   standalone: true,
-  imports: [],
+  imports: [
+    DefaultBtnComponent
+  ],
   templateUrl: './video-selection.component.html',
   styleUrl: './video-selection.component.css'
 })
