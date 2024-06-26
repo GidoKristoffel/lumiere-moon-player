beaf593dc978de450f03634896fe76a3dff6ff6d
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-26 07:24:29 +0300

    💄 Updated title bar styles

--- src/app/shared/components/buttons/icon-btn/icon-btn.component.ts
+++ src/app/shared/components/buttons/icon-btn/icon-btn.component.ts
@@ -1,22 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NgOptimizedImage } from "@angular/common";
+import { InlineSVGModule } from "ng-inline-svg-2";
+import { HttpClientModule } from "@angular/common/http";
 
 @Component({
-  selector: 'lmp-icon-btn',
-  standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
-  templateUrl: './icon-btn.component.html',
-  styleUrl: './icon-btn.component.css'
+    selector: 'lmp-icon-btn',
+    standalone: true,
+    imports: [InlineSVGModule, HttpClientModule],
+    templateUrl: './icon-btn.component.html',
+    styleUrl: './icon-btn.component.css'
 })
 export class IconBtnComponent {
-  @Input() btnSize: string = '';
-  @Input() icon: string = '';
-  @Input() iconSize: string = '';
-  @Output() click: EventEmitter<void> = new EventEmitter();
+    @Input() btnSize: string = '';
+    @Input() icon: string = '';
+    @Input() iconSize: string = '';
+    @Output() click: EventEmitter<void> = new EventEmitter();
 
-  public onClick(): void {
-    this.click.emit();
-  }
+    public onClick(): void {
+        this.click.emit();
+    }
 }
