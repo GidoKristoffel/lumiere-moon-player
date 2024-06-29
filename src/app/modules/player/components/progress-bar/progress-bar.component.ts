701719eb9f4fc5840ee4ccf84f2a8d2eac712977
DmitryR <dmytro.ryabchun@gmail.com>
2024-06-29 17:34:57 +0300

    ✨ Added logic of video progress bar

--- src/app/modules/player/components/progress-bar/progress-bar.component.ts
+++ src/app/modules/player/components/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { VideoControlService } from "../../services/video-control.service";
 
 @Component({
@@ -9,12 +9,31 @@ import { VideoControlService } from "../../services/video-control.service";
   styleUrl: './progress-bar.component.scss'
 })
 export class ProgressBarComponent {
+  @HostListener('window:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.videoControlService.onMouseMove(event, this.progressBarContainer.nativeElement);
+  }
+
+  @HostListener('window:mouseup')
+  onMouseUp() {
+    this.videoControlService.onMouseUp();
+  }
+
+  @ViewChild('progressBarContainer') progressBarContainer!: ElementRef<HTMLDivElement>;
   public buffered = this.videoControlService.buffered.asReadonly();
   public progress = this.videoControlService.progress.asReadonly();
 
   constructor(private videoControlService: VideoControlService) {}
 
   public seekVideo(event: MouseEvent): void {
-    this.videoControlService.videoControlService(event);
+    this.videoControlService.seekVideo(event);
+  }
+
+  public startSeeking(event: MouseEvent): void {
+    this.videoControlService.startSeeking(event);
+  }
+
+  public stopSeeking(): void {
+    this.videoControlService.stopSeeking();
   }
 }
