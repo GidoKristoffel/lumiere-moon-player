09be9a7398b65558c90663f83aa8558fb0a65286
DmitryR <dmytro.ryabchun@gmail.com>
2024-07-05 20:35:19 +0300

    ✨ Added video time tooltip component

---
+++ src/app/modules/player/components/time-tooltip/time-tooltip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lmp-time-tooltip',
+  standalone: true,
+  imports: [],
+  templateUrl: './time-tooltip.component.html',
+  styleUrl: './time-tooltip.component.scss'
+})
+export class TimeTooltipComponent {
+
+}
