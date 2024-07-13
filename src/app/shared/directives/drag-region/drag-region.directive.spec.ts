58de5d950775682507906d416608c077c7dd0e36
DmitryR <dmytro.ryabchun@gmail.com>
2024-07-13 20:31:12 +0300

    👔 Updated fullscreen logic

---
+++ src/app/shared/directives/drag-region/drag-region.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DragRegionDirective } from './drag-region.directive';
+
+describe('DragRegionDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DragRegionDirective();
+    expect(directive).toBeTruthy();
+  });
+});
