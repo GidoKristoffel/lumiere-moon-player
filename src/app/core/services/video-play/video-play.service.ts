59187602b58e7af675f8ce762a78f816a95e4c76
DmitryR <dmytro.ryabchun@gmail.com>
2024-07-14 15:13:24 +0300

    👔 Updated video play service

--- src/app/core/services/video-play/video-play.service.ts
+++ src/app/core/services/video-play/video-play.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, WritableSignal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoPlayService {
+  private playing: WritableSignal<boolean> = signal<boolean>(false);
+  private videoElement: HTMLVideoElement | undefined = undefined;
 
   constructor() { }
+
+  init(videoElement: HTMLVideoElement): void {
+    this.videoElement = videoElement;
+  }
 }
