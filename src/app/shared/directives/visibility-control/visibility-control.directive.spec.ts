2e9b300c6926160d29f2c0be0b79c15dcc91df3a
DmitryR <dmytro.ryabchun@gmail.com>
2024-07-25 16:18:24 +0300

    ✨ Added visibility control directive

---
+++ src/app/shared/directives/visibility-control/visibility-control.directive.spec.ts
@@ -0,0 +1,8 @@
+import { VisibilityControlDirective } from './visibility-control.directive';
+
+describe('VisibilityControlDirective', () => {
+  it('should create an instance', () => {
+    const directive = new VisibilityControlDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/directives/visibility-control/visibility-control.directive.ts
@@ -0,0 +1,47 @@
+import { Directive, ElementRef, HostListener, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+
+@Directive({
+  selector: '[lmpVisibilityControl]',
+  standalone: true
+})
+export class VisibilityControlDirective implements OnChanges {
+  @Input() displayCondition: boolean = true;
+  @Input() timeUntilExtinction: number = 2000;
+  @Input() element: ElementRef | undefined;
+
+  private isCondition: boolean = true;
+  private hideTimeout: any;
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(): void {
+    if (this.isCondition) {
+      this.setVisible(true);
+      clearTimeout(this.hideTimeout);
+      this.hideTimeout = setTimeout(() => this.setVisible(false), this.timeUntilExtinction);
+    }
+  }
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['displayCondition']) {
+      this.isCondition = changes['displayCondition'].currentValue;
+      if (this.isCondition) {
+        this.setVisible(false);
+      } else {
+        this.setVisible(true);
+      }
+    }
+  }
+
+  private setVisible(visible: boolean): void {
+    if (this.element) {
+      if (visible) {
+        this.renderer.setStyle(this.element.nativeElement, 'display', 'block');
+      } else {
+        this.renderer.setStyle(this.element.nativeElement, 'display', 'none');
+      }
+    }
+  }
+
+}
