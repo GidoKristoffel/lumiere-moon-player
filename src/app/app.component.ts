4b3c269fab133ff08299763beec4c2dccfe6be2e
DmitryR <dmytro.ryabchun@gmail.com>
2024-07-28 21:14:58 +0300

    🔥 Removed unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ControlPanelComponent } from "./modules/player/components/control-panel/control-panel.component";
 import { TitleBarComponent } from "./shared/components/title-bar/title-bar.component";
-import { FullscreenWindowService } from "./core/services/fullscreen-window/fullscreen-window.service";
 import { HotkeysService } from "./core/services/hotkeys/hotkeys.service";
-import { FullscreenVideoService } from "./core/services/fullscreen-video/fullscreen-video.service";
 import { FullscreenVideoStatusService } from "./core/services/fullscreen-video-status/fullscreen-video-status.service";
 import { NoFocusService } from "./core/services/no-focus/no-focus.service";
 import { MaximizedWindowStatusService } from "./core/services/maximized-window-status/maximized-window-status.service";
@@ -19,13 +17,11 @@ import { MaximizedWindowStatusService } from "./core/services/maximized-window-s
 })
 export class AppComponent {
   constructor(
-      // private fullscreenWindowService: FullscreenWindowService,
       private hotkeysService: HotkeysService,
       private fullscreenVideoStatusService: FullscreenVideoStatusService,
       private noFocusService: NoFocusService,
       private maximizedWindowStatusService: MaximizedWindowStatusService
   ) {
-    // this.fullscreenWindowService.init();
     this.maximizedWindowStatusService.init();
     this.hotkeysService.init();
     this.fullscreenVideoStatusService.init();
--- src/app/core/services/fullscreen-window/fullscreen-window.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, Signal, signal, WritableSignal } from '@angular/core';
-import { appWindow } from "@tauri-apps/api/window";
-
-@Injectable({
-  providedIn: 'root'
-})
-export class FullscreenWindowService {
-
-}
