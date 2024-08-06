2cff0e40e5e0f2f15b11c089bea2f7ef3a034475
DmitryR <dmytro.ryabchun@gmail.com>
2024-08-06 22:50:13 +0300

    ✅ Added tests

--- src/app/modules/player/classes/watch-video-element-ready/watch-video-element-ready.spec.ts
+++ src/app/modules/player/classes/watch-video-element-ready/watch-video-element-ready.spec.ts
@@ -1,17 +1,41 @@
 import { WatchVideoElementReady } from './watch-video-element-ready';
 import { of } from "rxjs";
+import { TestBed } from "@angular/core/testing";
+import { VideoService } from "../../../../core/services/video/video.service";
 
 export class MockVideoService {
   onElementReady = of(null);
 
   getElement(): HTMLVideoElement {
-    const videoElement = document.createElement('video');
-    return videoElement;
+    return document.createElement('video');
   }
 }
 
 describe('WatchVideoElementReady', () => {
-  it('should create an instance', () => {
-    expect(new WatchVideoElementReady()).toBeTruthy();
+  let watchVideoElementReady: WatchVideoElementReady;
+  let videoService: MockVideoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: VideoService, useClass: MockVideoService }
+      ]
+    });
+
+    videoService = TestBed.inject(VideoService) as unknown as MockVideoService;
+
+    // Создание тестового экземпляра абстрактного класса
+    class TestWatchVideoElementReady extends WatchVideoElementReady {
+      constructor(protected override videoService: VideoService) {
+        super(videoService);
+      }
+    }
+
+    watchVideoElementReady = new TestWatchVideoElementReady(videoService);
+  });
+
+  it('should set videoElement when onElementReady emits', () => {
+    expect(watchVideoElementReady.videoElement).toBeDefined();
+    expect(watchVideoElementReady.videoElement instanceof HTMLVideoElement).toBe(true);
   });
 });
