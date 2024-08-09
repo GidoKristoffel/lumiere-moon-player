60e70fec91ea26288d3f42e783ba04db57131766
DmitryR <dmytro.ryabchun@gmail.com>
2024-08-09 20:35:40 +0300

    ✅ Added tests

--- src/app/modules/player/services/video-buffering-view/video-buffering-view.service.spec.ts
+++ src/app/modules/player/services/video-buffering-view/video-buffering-view.service.spec.ts
@@ -1,13 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VideoBufferingViewService } from './video-buffering-view.service';
+import { VideoService } from "../../../../core/services/video/video.service";
 
 describe('VideoBufferingViewService', () => {
   let service: VideoBufferingViewService;
+  let videoServiceSpy: jasmine.SpyObj<VideoService>;
+  let mockVideoElement: HTMLVideoElement;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    videoServiceSpy = jasmine.createSpyObj('VideoService', ['getElement', 'onElementReady']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        VideoBufferingViewService,
+        { provide: VideoService, useValue: videoServiceSpy }
+      ]
+    });
+
     service = TestBed.inject(VideoBufferingViewService);
+
+    // Mock HTMLVideoElement
+    mockVideoElement = document.createElement('video');
+    spyOnProperty(service, 'videoElement', 'get').and.returnValue(mockVideoElement);
   });
 
   it('should be created', () => {
