bb8269932420a2d88f4212ca70e66c8767e93d77
DmitryR <dmytro.ryabchun@gmail.com>
2024-08-11 20:27:02 +0300

    ✅ Added tests

--- src/app/core/services/fullscreen-video-status/fullscreen-video-status.service.spec.ts
+++ src/app/core/services/fullscreen-video-status/fullscreen-video-status.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FullscreenVideoStatusService } from './fullscreen-video-status.service';
+import { LocalStorageService } from "../local-storage/local-storage.service";
+import { FullscreenVideoService } from "../fullscreen-video/fullscreen-video.service";
 
 describe('FullscreenVideoStatusService', () => {
   let service: FullscreenVideoStatusService;
+  let localStorageService: jasmine.SpyObj<LocalStorageService>;
+  let fullscreenVideoService: jasmine.SpyObj<FullscreenVideoService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const localStorageSpy = jasmine.createSpyObj('LocalStorageService', ['get', 'set']);
+    const fullscreenSpy = jasmine.createSpyObj('FullscreenVideoService', ['on', 'off']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        FullscreenVideoStatusService,
+        { provide: LocalStorageService, useValue: localStorageSpy },
+        { provide: FullscreenVideoService, useValue: fullscreenSpy }
+      ]
+    });
+
     service = TestBed.inject(FullscreenVideoStatusService);
+    localStorageService = TestBed.inject(LocalStorageService) as jasmine.SpyObj<LocalStorageService>;
+    fullscreenVideoService = TestBed.inject(FullscreenVideoService) as jasmine.SpyObj<FullscreenVideoService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
 });
