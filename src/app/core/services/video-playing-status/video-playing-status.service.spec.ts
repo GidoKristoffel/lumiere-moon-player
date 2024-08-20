2358183f3c3da8a868e1853775bb75593daeedf9
DmitryR <dmytro.ryabchun@gmail.com>
2024-08-20 20:00:00 +0300

    ✅ Added tests

--- src/app/core/services/video-playing-status/video-playing-status.service.spec.ts
+++ src/app/core/services/video-playing-status/video-playing-status.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VideoPlayingStatusService } from './video-playing-status.service';
+import { Signal } from "@angular/core";
 
 describe('VideoPlayingStatusService', () => {
   let service: VideoPlayingStatusService;
@@ -12,5 +13,10 @@ describe('VideoPlayingStatusService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return the initial status as false', () => {
+    const statusSignal: Signal<boolean> = service.watch();
+    expect(statusSignal()).toBe(false);
   });
 });
