6021df99062fd23b94145e6000857ab515834cce
DmitryR <dmytro.ryabchun@gmail.com>
2024-08-27 20:20:21 +0300

    ✅ Added tests

--- src/app/core/services/window/window.service.spec.ts
+++ src/app/core/services/window/window.service.spec.ts
@@ -3,6 +3,18 @@ import { TestBed } from '@angular/core/testing';
 import { WindowService } from './window.service';
 import { MaximizedWindowStatusService } from "../maximized-window-status/maximized-window-status.service";
 
+// Заглушаем методы appWindow
+jest.mock('@tauri-apps/api/window', () => ({
+  appWindow: {
+    minimize: jest.fn().mockResolvedValue(undefined),
+    isMaximized: jest.fn().mockResolvedValue(false),
+    maximize: jest.fn().mockResolvedValue(undefined),
+    unmaximize: jest.fn().mockResolvedValue(undefined),
+    close: jest.fn().mockResolvedValue(undefined),
+    startDragging: jest.fn().mockResolvedValue(undefined),
+  }
+}));
+
 describe('WindowService', () => {
   let service: WindowService;
   let maximizedWindowStatusService: MaximizedWindowStatusService;
