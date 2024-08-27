9f14f266c074cb0a29d7636630b32a124f2a021c
DmitryR <dmytro.ryabchun@gmail.com>
2024-08-27 20:19:23 +0300

    ✅ Added tests

--- src/app/core/services/window/window.service.spec.ts
+++ src/app/core/services/window/window.service.spec.ts
@@ -1,16 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WindowService } from './window.service';
+import { MaximizedWindowStatusService } from "../maximized-window-status/maximized-window-status.service";
 
 describe('WindowService', () => {
   let service: WindowService;
+  let maximizedWindowStatusService: MaximizedWindowStatusService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MaximizedWindowStatusService]
+    });
     service = TestBed.inject(WindowService);
+    maximizedWindowStatusService = TestBed.inject(MaximizedWindowStatusService);
+
+    // Заглушаем метод set в MaximizedWindowStatusService
+    jest.spyOn(maximizedWindowStatusService, 'set').mockImplementation(() => {});
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 });
